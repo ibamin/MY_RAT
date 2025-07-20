f63f1ca6482a6071767a075d50ed210168531bd0
ibamin <rob990628@gmail.com>
2025-07-21 00:12:24 +0900

    "Local Com Execute ADD"

---
+++ Agent/target/debug/Agent.d
@@ -0,0 +1,1 @@
+C:\Users\shrud\Desktop\Projects\MY_RAT\MY_RAT\Agent\target\debug\Agent.exe: C:\Users\shrud\Desktop\Projects\MY_RAT\MY_RAT\Agent\src\main.rs
---
+++ Agent/target/debug/deps/Agent.d
@@ -0,0 +1,5 @@
+C:\Users\shrud\Desktop\Projects\MY_RAT\MY_RAT\Agent\target\debug\deps\Agent.exe: src\main.rs
+
+C:\Users\shrud\Desktop\Projects\MY_RAT\MY_RAT\Agent\target\debug\deps\Agent.d: src\main.rs
+
+src\main.rs:
